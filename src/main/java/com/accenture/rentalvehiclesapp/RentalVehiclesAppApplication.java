fb4779a513724dbe46c7c89dee368a33c65a1343
murciana.le.bouedec <murciana.lb@gmail.com>
2026-02-26 14:04:42 +0100

    first commit

---
+++ src/main/java/com/accenture/rentalvehiclesapp/RentalVehiclesAppApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.rentalvehiclesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalVehiclesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalVehiclesAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/accenture/rentalvehiclesapp/RentalVehiclesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.rentalvehiclesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalVehiclesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
